1114db2bfbfb25d4844cc5cf389e96a7df570caf
kohei-iwase <me.tekelily@gmail.com>
2020-01-31 04:06:20 +0000

    [Finish]2

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-aleart('Hello World')
+// var max = 100;
+//var num = 1;
+//var count = 0;
+//while(num < max){
+//	num = num * 2;
+//	count = count + 1;
+//}
+
+//alert('2をかけ続けて'　+ max + 'を越えるのに必要だった回数は'　+ count + '回です')
+
+var i;
+var num = 0;
+
+for(i = 1; i < 11; i++){
+	num = num + i;
+}
+alert('１から１０までを足した和は'+ num + 'です');
